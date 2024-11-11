72e9ce190637d648d5eabeaa47d9dbab3ed7e6c3
AdrianPerez2207 <aperezsanchez441@gmail.com>
2024-11-11 12:46:42 +0100

    proyecto iniciado

---
+++ source/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace Coworking;
+    session_start();
+
+    /**
+     * AUTOLOAD
+     */
+    spl_autoload_register(function ($class) {
+        //echo $class."<br>";
+        //echo substr($class, strpos($class,"\\")+1);
+        $ruta = substr($class, strpos($class,"\\")+1);
+        $ruta = str_replace("\\", "/", $ruta);
+        include_once "./" . $ruta . ".php";
+    });
